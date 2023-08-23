8d08aa7e72b76edfe860d6cc8d19f951e76e676b
Grace <gracesilvaduarte@gmail.com>
2023-08-23 17:40:18 -0500

    Cambio de Imagenes Listo

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ function showUserName(){
     let nameElement = document.getElementById("nameUser");
     let titleElement = document.getElementById("mainTitle");
 
-    titleElement.textContent = "You've had a terrible fate, haven't you, " + nameElement.value + "?";
+    titleElement.textContent = nameElement.value + " YOU HAVE HAD A TERRIBLE FATE, HAVEN'T YOU?";
+
+    document.querySelector("input").style.display="none";
+    document.getElementById("majorasMask").style.display="none";
+    document.getElementById("maskSeller").style.display="block";
+
+
 }
 
 /** You've met a terrible fate, haven't you? */
