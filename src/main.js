ae923fd1b79c1afd6840648589e653f1ca1eed74
Grace <gracesilvaduarte@gmail.com>
2023-08-23 19:15:38 -0500

    se agregó una función de delay para mejorar la transición entre pantallas

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
-function showUserName(){
+function delay(time){
+
+    return new Promise(resolve => setTimeout(resolve, time));
+        
+}
+
+async function showUserName(){
     let nameElement = document.getElementById("nameUser");
     let titleElement = document.getElementById("mainTitle");
+    
+    
 
-    titleElement.textContent = nameElement.value + " YOU HAVE HAD A TERRIBLE FATE, HAVEN'T YOU?";
-
-    document.querySelector("input").style.display="none";
+    document.querySelector(".nameUserInput").style.display="none";
     document.getElementById("majorasMask").style.display="none";
+    document.getElementById("buttonUser").style.display="none";
+    document.querySelector(".all-container").style="background: #000";
+    document.getElementById("mainTitle").style.display="none";
+    await delay(1500);
+
+
     document.getElementById("maskSeller").style.display="block";
+    titleElement.textContent = nameElement.value + ", You have had a terrible fate, haven't you?";
+    document.getElementById("mainTitle").style.display="block";
 
 
 }
