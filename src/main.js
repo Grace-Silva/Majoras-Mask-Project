4b189e4ceade5e3c742602471a99659e17ca7011
Grace <gracesilvaduarte@gmail.com>
2023-08-23 19:19:34 -0500

    el fondo retoma su degradado original

--- src/main.js
+++ src/main.js
@@ -17,7 +17,8 @@ async function showUserName(){
     document.getElementById("mainTitle").style.display="none";
     await delay(1500);
 
-
+    document.querySelector(".all-container").style="background: radial-gradient(circle, rgba(14,9,9,1) 0%, rgba(31,28,28,1) 25%, rgba(16,15,15,1) 50%, rgba(10,10,10,1) 75%, rgba(4,4,4,1) 100%)";
+    
     document.getElementById("maskSeller").style.display="block";
     titleElement.textContent = nameElement.value + ", You have had a terrible fate, haven't you?";
     document.getElementById("mainTitle").style.display="block";
